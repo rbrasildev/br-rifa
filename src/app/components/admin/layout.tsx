2f34b7ee8e70908bd3f996c129a1474a89a672e5
rbrasildev <rairockbrasil@hotmail.com>
2024-02-20 15:41:48 -0300

    motion

--- src/app/components/admin/layout.tsx
+++ src/app/components/admin/layout.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { motion, AnimatePresence } from "framer-motion"
+import { motion } from "framer-motion"
 import { ReactNode, useState } from 'react';
 import Sidebar from './sidebar';
 import Header from './header';
@@ -12,19 +12,24 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
   const [isOpen, setIsOpen] = useState(false)
   return (
     <div className="wrap relative">
+
+      <Sidebar
+        classes={isOpen ? 'block' : 'max-sm:hidden max-md:hidden max-lg:hidden'}
+      />
+
       <button
         onClick={() => setIsOpen(!isOpen)}
         className='absolute left-4 top-5 text-2xl hidden max-sm:block max-md:block z-30 max-lg:block'>
         <LuAlignLeft />
       </button>
-      <Sidebar
-        classes={isOpen ? 'visible' : 'max-sm:invisible max-md:invisible max-lg:invisible'}
-      />
+
       <Header />
-      <main
+      <motion.main
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         className="max-md:ml-0 max-lg:ml-0 max-sm:p-4 max-md:p-4 max-lg:p-4 p-16 ml-[250px]">
         {children}
-      </main>
+      </motion.main>
     </div >
   );
 }
