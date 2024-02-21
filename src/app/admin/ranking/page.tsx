b24ac9ccf7403bf81d9160a68423e3e69d69ec98
rbrasildev <rairockbrasil@hotmail.com>
2024-02-21 12:28:52 -0300

    ranking

--- src/app/admin/ranking/page.tsx
+++ src/app/admin/ranking/page.tsx
@@ -1,20 +1,47 @@
+'use client'
 import DashboardLayout from "@/app/components/admin/layout"
-import { LuChevronUp } from "react-icons/lu"
+import { useEffect, useState } from "react";
 
+
+interface userProps {
+    nome: string,
+    endereco: string,
+    bairro: string,
+    telefone: string,
+    created_at: string
+}
 export default function Ranking() {
+    const [user, setUser] = useState<userProps[]>([]);
+    useEffect(() => {
+        fetch('https://fscode.com.br/api/employer')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (response) {
+                setUser(response)
+            })
+            .then((error) => {
+                console.log(error)
+            })
+    }, [])
+
+
     return (
         <DashboardLayout>
-            <h1>Raking</h1>
-            <div>
-                <div className="flex p-6 border rounded-lg shadow">
-                    <ul>
-                        <li>Nome</li>
-                        <li>Endereço</li>
-                        <li>CPF</li>
-                        <li>Telefone</li>
-                        <li>Data</li>
-                    </ul>
-                </div>
+
+            <div className="flex gap-2 flex-wrap">
+                {user.map(item => {
+                    return <div className="flex p-6 border rounded-lg shadow">
+                        <ul>
+                            <li>{item.nome}</li>
+                            <li>{item.endereco}</li>
+                            <li>{item.bairro}</li>
+                            <li>{item.telefone}</li>
+                            <li>{item.created_at}</li>
+                        </ul>
+                    </div>
+                })}
+
             </div>
 
         </DashboardLayout >
