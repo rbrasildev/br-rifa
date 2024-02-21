1f7d861970285ebd0f54d760152a7d38908f2597
Jacintho George Ficagna <jacinthogfdev@gmail.com>
2024-02-21 13:45:29 -0300

    prisma config

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
 export async function GET() {
-  return Response.json({
-    ok: true
+  const newUser = await prisma.user.create({
+    data: {
+      name: 'Elliott',
+      email: 'xelliottx@example-user.com'
+    }
   });
+
+  const users = await prisma.user.findMany();
+
+  return Response.json(users);
 }
 
 export async function POST() {
